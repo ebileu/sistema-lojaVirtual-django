360bf4c0c7b96e141ab72f55bea732601246e1ab
ebileu <anjogabriels2@hotmail.com>
2025-03-08 16:12:12 -0300

    Adição de template de cadastro, criação de produtos e listagem

--- loja_virtual/urls.py
+++ loja_virtual/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('pagina-inicial', views.pagina_inicial, name='pagina_inicial')
+    path('pagina-inicial', views.pagina_inicial, name='pagina_inicial'),
+    path('cadastro-produto', views.cadastro_produto, name='cadastro_produto')
 ]
